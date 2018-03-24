a55c438631173a9d50d2c61d962d6f8c3363cbbe
feeroz123 <feeroz123@gmail.com>
2018-03-24 23:51:25 +0530

    Reverse words in a sentence

---
+++ ReverseWordOrder.py
@@ -0,0 +1,9 @@
+# Write a program (using functions!) that asks the user for a long string containing multiple words.
+# Print back to the user the same string, except with the words in backwards order.
+
+ustring = input("Enter a sentence: ")
+
+splitString = ustring.split(" ")        # Separate the words by " " character
+splitString.reverse()                   # Perform reverse operation on the string
+
+print(" ".join(splitString))            # Stich/Merge the words with " " character in between them
