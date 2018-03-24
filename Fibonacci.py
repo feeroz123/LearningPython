93cfd1cf53d457aa6e1d9df6f05f4106325f310d
feeroz123 <feeroz123@gmail.com>
2018-03-24 15:03:39 +0530

    Fibonacci series exercise

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+# Write a program that asks the user how many Fibonnaci numbers to generate and then generates them
+
+def getfib(num):
+    a = 0
+    b = 1
+    print(a)
+    print(b)
+    for i in range (0,num):
+        c = a + b
+        print(c)
+        a = b
+        b = c
+
+
+unum = int(input("Enter the number of elements to generate in Fibonacci series: "))
+getfib(unum)
