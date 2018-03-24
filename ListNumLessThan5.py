75aacf2142c80e63a38f731d8ce6cd81d3227f26
feeroz123 <feeroz123@gmail.com>
2018-03-24 15:50:53 +0530

    List with numbers less than 5

--- ListNumLessThan5.py
+++ ListNumLessThan5.py
@@ -4,8 +4,9 @@ count = int(input("Enter the size of your list: "))
 ulist = []
 
 for i in range (0, count):
-    ulist.append(input("Enter value: "))
-
+    ulist.append(int(input("Enter value: ")))
 
 print ("Entered list items: ", ulist)
+print("Values less than 5 are: ")
+print ([j for j in ulist if j < 5])
 
