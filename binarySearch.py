91bb75e6654eed0f5f4cbed9f0e7aa8cfcf76408
Feeroz Alam <feeroz123@gmail.com>
2018-04-05 22:35:04 +0530

    Updated Binary search code

--- binarySearch.py
+++ binarySearch.py
@@ -2,28 +2,30 @@
 # The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
 
 def search (inputList, searchValue):
-        startIndex = 0
-        endIndex = len(inputList)-1
-        while True:
-            midIndex = int((startIndex+endIndex)/2)
-            midValue = inputList[midIndex]
+    startIndex = 0
+    endIndex = len(inputList) - 1
 
-            if midValue == searchValue or inputList[startIndex] == searchValue or inputList[endIndex] == searchValue:
-                return True
-                break
-            if midValue == 0 or midIndex == startIndex or midIndex == endIndex:
-                return False
-                break
-            elif midValue > searchValue:
-                endIndex == midIndex
-            else:
-                startIndex = midIndex
+    while True:
+        midIndex = int((startIndex+endIndex)/2)
+        midValue = inputList[midIndex]
+
+        if midValue == searchValue or inputList[startIndex] == searchValue or inputList[endIndex] == searchValue:
+            return True
+            break
+        if midValue == 0 or midIndex == startIndex or midIndex == endIndex:
+            return False
+            break
+        elif midValue > searchValue:
+            endIndex = midIndex
+        else:
+            startIndex = midIndex
 
 
-ourList = [1,2,4,6,9,12,16]
+ourList = [1,2,4,6,9,12,16,17,19]
 
 userInput = int(input("Enter a number to search: "))
 
 if search(ourList, userInput):
     print("Found")
-else: print("Not Found")
+else:
+    print("Not Found")
