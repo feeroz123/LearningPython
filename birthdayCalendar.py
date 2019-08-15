0f22c073d9006df8549b78ce9ec5dd8357ab63f4
feeroz.alam <feeroz.alam@gmail.com>
2019-08-15 17:44:48 +0530

    cosmetic change to birthdayCalendar.py

--- birthdayCalendar.py
+++ birthdayCalendar.py
@@ -1,6 +1,6 @@
 # When you run your program it should ask the user to enter a name, and return the birthday of that person back to them
 
-calendar = {"Feeroz":"15/02/1986", "Hina":"10/11/1987", "Huma":"12/08/1991", "Nisha":"13/06/1984"}
+calendar = {"Feeroz":"18/09/1981", "Hina":"12/01/1987", "Huma":"02/10/1997", "Nisha":"19/10/1980"}
 
 print("We have birthday dates of Feeroz, Hina, Huma and Nisha")
 uname = input("Whose birthday do you want? Enter name: ")
