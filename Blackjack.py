3e188d9962de5b0da7d053a9fce43190ff94768f
feeroz.alam <feeroz.alam@gmail.com>
2019-08-15 17:59:16 +0530

    fixed a bug when all numbers are 11

--- Blackjack.py
+++ Blackjack.py
@@ -1,5 +1,5 @@
 # BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum.
-# If their sum exceeds 21 and there's an eleven, reduce the total sum by 10.
+# If their sum exceeds 21 and there's an 11 in entered numbers, reduce the total sum by 10.
 # Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
 
 def blackjack(a, b, c):
@@ -7,9 +7,13 @@ def blackjack(a, b, c):
     if sum <= 21:
         result = sum
     elif a == 11 or b == 11 or c == 11:
-        result = sum - 10
+        sum = sum - 10
+        if sum > 21:
+            result = "BUST"
+        else:
+            result = sum
     else:
-        result = 'BUST'
+        result = "BUST"
 
     print(result)
 
@@ -17,3 +21,4 @@ def blackjack(a, b, c):
 blackjack(5,6,7) #--> 18
 blackjack(9,9,9) #--> 'BUST'
 blackjack(9,9,11) #--> 19
+blackjack(11,11,11) #--> 'BUST'
