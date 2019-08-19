eeae0e3ed3a7765d0633e144daba79e4f5920d0a
feeroz.alam <feeroz.alam@gmail.com>
2019-08-19 20:52:56 +0530

    Added problem definition

--- primeNumber.py
+++ primeNumber.py
@@ -1,10 +1,12 @@
+# Function to find if entered number is Prime or Composite
+
 u = 0
 flag = 0
 
 while u < 2:
     u = int(input("Enter a number: "))
 
-if u > 1:
+if u > 2:
    for i in range(2, u):
         if u % i == 0:
             flag = 1
